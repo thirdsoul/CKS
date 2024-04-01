a73c994a883cb249da9770f3f0ff7fb7708f24d2
Antonio Trapero <antoniotrapero@gmail.com>
2024-04-01 12:12:05 +0100

    Auth RBAC examples

---
+++ authz/create-user.sh
@@ -0,0 +1,26 @@
+#Create user able to list namespaces and pods in all the clusters, 
+#but have all permissions in test namespace
+#ns creation
+kubectl create ns test
+
+#sa creation
+kubectl create serviceaccount test-admin -n test;
+
+#role creations
+kubectl create clusterrole test-viewer --resource namespaces,pods,deploy --verb list,get;
+kubectl create clusterrole test-admin --resource=* --verb=*;
+
+#rolebindings, important to limit admin rights in namespace scope with a simple 
+#rolebinding instead of clusterrolebinding
+kubectl create clusterrolebinding test-viewer-binding --clusterrole test-viewer --serviceaccount test:test-admin;
+kubectl create rolebinding test-admin-binding --clusterrole test-admin --serviceaccount test:test-admin -n test;
+
+#validate permissions
+kubectl auth can-i list pods --as system:serviceaccount:test:test-admin -n kube-system;
+kubectl auth can-i delete pods --as system:serviceaccount:test:test-admin -n kube-system;
+kubectl auth can-i delete pods --as system:serviceaccount:test:test-admin -n test;
+
+
+
+
+
